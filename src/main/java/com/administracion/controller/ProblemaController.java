a42643eb76a3e6a47b72bace3ef6bc00720b503f
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-11-21 13:56:40 -0500

    Context path added '/api'

--- src/main/java/com/administracion/controller/ProblemaController.java
+++ src/main/java/com/administracion/controller/ProblemaController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/problema")
+@RequestMapping("/api/problema")
 public class ProblemaController {
 
     @Autowired
